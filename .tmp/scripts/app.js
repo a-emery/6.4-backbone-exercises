314d138e724b1280c2ae0100aef14d74aa604d9c
Aaron Emery <aemery14@gmail.com>
2015-10-01 14:11:23 -0400

    File structure created

--- .tmp/scripts/app.js
+++ .tmp/scripts/app.js
@@ -5,4 +5,39 @@ require.register('main', function (exports, require, module) {
         $('#container').append(JST.application());
     });
 });
+require.register('a/a-main', function (exports, require, module) {
+    'use strict';
+    $(document).ready(function () {
+        // prepend the contents of `app/templates/application.hbs` into `body`
+        $('#container').append(JST['a/a-index']());
+    });
+});
+require.register('b/b-main', function (exports, require, module) {
+    'use strict';
+    $(document).ready(function () {
+        // prepend the contents of `app/templates/application.hbs` into `body`
+        $('#container').append(JST['b/b-index']());
+    });
+});
+require.register('c/c-main', function (exports, require, module) {
+    'use strict';
+    $(document).ready(function () {
+        // prepend the contents of `app/templates/application.hbs` into `body`
+        $('#container').append(JST['c/c-index']());
+    });
+});
+require.register('d/d-main', function (exports, require, module) {
+    'use strict';
+    $(document).ready(function () {
+        // prepend the contents of `app/templates/application.hbs` into `body`
+        $('#container').append(JST['d/d-index']());
+    });
+});
+require.register('e/e-main', function (exports, require, module) {
+    'use strict';
+    $(document).ready(function () {
+        // prepend the contents of `app/templates/application.hbs` into `body`
+        $('#container').append(JST['e/e-index']());
+    });
+});
 //# sourceMappingURL=app.js.map
--- .tmp/scripts/templates.js
+++ .tmp/scripts/templates.js
@@ -1,4 +1,19 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "<h1>hello</h1>\n";
+},"useData":true});
+this["JST"]["a/a-index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Create a Blog Post</h1>\n\n<form action=\"\">\n  <input type=\"text\">\n  <input type=\"text\">\n  <input type=\"submit\">\n</form>\n";
+},"useData":true});
+this["JST"]["b/b-index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Page B</h1>\n";
+},"useData":true});
+this["JST"]["c/c-index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Page C</h1>\n";
+},"useData":true});
+this["JST"]["d/d-index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Page D</h1>\n";
+},"useData":true});
+this["JST"]["e/e-index"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Page E</h1>\n";
 },"useData":true});
